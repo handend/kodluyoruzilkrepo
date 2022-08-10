924d8d6480df424363f7eeb73257ed3cec200a1f
handend <hnurdgn00@gmail.com>
2022-08-10 22:13:45 +0300

    son

--- csharp101/operatorler/Program.cs
+++ csharp101/operatorler/Program.cs
@@ -73,7 +73,7 @@ namespace operatorler
             Console.WriteLine(sonuc1);
             sonuc1= sayi1+sayi2;
             Console.WriteLine(sonuc1);
-            sonuc1 =sayi1++;
+            sonuc1 =++sayi1;
             Console.WriteLine(sonuc1);
 
             // % mod alır
