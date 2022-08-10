e30abb32bef7e1f83953f0948f87cdcdc5f62107
handend <hnurdgn00@gmail.com>
2022-08-10 22:11:02 +0300

    last one

--- csharp101/operatorler/Program.cs
+++ csharp101/operatorler/Program.cs
@@ -39,14 +39,51 @@ namespace operatorler
             
             // İlişkisel Operatörler
             // < küçüktür, > büyüktür,  <= küçük eşittir,  >= büyük eşittir, == eşit eşittir , != eşit değildir
-
+            
+            Console.WriteLine("*********İlişkisel Operatorler***********************");
+            
             int a= 3;
             int b=4;
             bool sonuc= a<b;
 
             Console.WriteLine(sonuc);
             
+            sonuc = a>b;
+            Console.WriteLine(sonuc);
+            sonuc= a>=b;
+            Console.WriteLine(sonuc);
+            sonuc= a<=b;
+            Console.WriteLine(sonuc);
+            sonuc= a==b;
+            Console.WriteLine(sonuc);
+            sonuc= a!=b;
+            Console.WriteLine(sonuc);
             
+            Console.WriteLine("*********Aritmetik Operatorler***********************");
+
+            // bölü / , çarpı * , artı +, eksi -
+
+            int sayi1= 10;
+            int sayi2= 5;
+
+            int sonuc1 = sayi1/sayi2 ;
+            Console.WriteLine(sonuc1);
+
+            sonuc1 = sayi1*sayi2;
+            Console.WriteLine(sonuc1);
+            sonuc1= sayi1+sayi2;
+            Console.WriteLine(sonuc1);
+            sonuc1 =sayi1++;
+            Console.WriteLine(sonuc1);
+
+            // % mod alır
+
+            int sonuc2 = 20%3 ;
+            Console.WriteLine(sonuc2);
+
+
+
+
 
         
         }
